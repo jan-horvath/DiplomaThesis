55c585bcce213eb35234bc3c992d2db329b03ad2
Ján Horváth <xhorvat6@fi.muni.cz>
2019-11-10 16:56:54 +0100

    Data file parsing is now working

---
+++ src/DataLoader.java
@@ -0,0 +1,64 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class DataLoader {
+    static private String matchFirstRegex(Pattern pattern, String string) {
+        Matcher matcher = pattern.matcher(string);
+        if (!matcher.find()) {
+            throw new InputMismatchException("Pattern was not found in string.");
+        }
+        return matcher.group(1);
+    }
+
+    static public Map<Integer, List<Integer>> parseGroundTruthFile(String filename) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader(new File(filename)));
+        Map<Integer, List<Integer>> groundTruth = new HashMap<>();
+
+        String line;
+        Pattern sequenceIdPattern = Pattern.compile("^(\\d+?)_");
+        Pattern classIdPattern = Pattern.compile("_(\\d+?)_");
+
+        while ((line = br.readLine()) != null) {
+            Integer sequenceId = Integer.parseInt(matchFirstRegex(sequenceIdPattern, line));
+            int classId = Integer.parseInt(matchFirstRegex(classIdPattern, line));
+
+            if (!groundTruth.containsKey(sequenceId)) {
+                groundTruth.put(sequenceId, new ArrayList<>());
+            }
+            groundTruth.get(sequenceId).add(classId);
+        }
+        return groundTruth;
+    }
+
+    static public Map<Integer, List<Integer>> parseDataFile(String filename) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(new File(filename)));
+        Map<Integer, List<Integer>> motions = new HashMap<>();
+
+        String line;
+        Pattern sequenceIdPattern = Pattern.compile("ObjectKey (\\d+?)_");
+        Pattern motionWordsCountPattern = Pattern.compile("^(\\d+?);");
+
+        while ((line = bufferedReader.readLine()) != null) {
+            if (line.contains("#objectKey")) {
+
+                Integer sequenceId = Integer.parseInt(matchFirstRegex(sequenceIdPattern, line));
+                line = bufferedReader.readLine();
+                int motionWordsCount = Integer.parseInt(matchFirstRegex(motionWordsCountPattern, line));
+
+                List<Integer> motionWords = new ArrayList<>();
+                for (int i = 0; i < motionWordsCount; ++i) {
+                    line = bufferedReader.readLine();
+                    motionWords.add(Integer.parseInt(line));
+                }
+
+                motions.put(sequenceId, motionWords);
+            }
+        }
+        return motions;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,76 +1,31 @@
 import java.io.*;
-import java.util.*;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
+import java.util.List;
+import java.util.Map;
 
 public class Main {
-    static private File groundTruthFile = new File(System.getProperty("user.dir") + "\\MW_database\\ground_truth_test.txt");
-    //static private File groundTruthFile = new File(System.getProperty("user.dir") + "\\MW_database\\ground_truth-sequence_actions.txt");
-    static private File dataFile = new File(System.getProperty("user.dir") + "\\MW_database\\hdm05-sequences_annotations_specific-segment80_shift16-coords_normPOS-fps12-quantized-kmedoids350.data");
+    static private String groundTruthFile = System.getProperty("user.dir") + "\\MW_database\\ground_truth-sequence_actions.txt";
+    static private String dataFile = System.getProperty("user.dir") + "\\MW_database\\hdm05-sequences_annotations_specific-segment80_shift16-coords_normPOS-fps12-quantized-kmedoids350.data";
     static public final int MIN_CLASS = 22;
     static public final int MAX_CLASS = 152;
 
-    static private Map<Integer, int[]> parseGroundTruthFile() throws IOException {
-        BufferedReader br = new BufferedReader(new FileReader(groundTruthFile));
-        Map<Integer, int[]> groundTruth = new HashMap<>();
-
-        String line;
-        Pattern sequenceIdPattern = Pattern.compile("^(\\d+?)_");
-        Pattern classIdPattern = Pattern.compile("_(\\d+?)_");
-        Matcher sequenceIdMatcher, classIdMatcher;
-        while ((line = br.readLine()) != null) { //TODO refactor parsing
-            sequenceIdMatcher = sequenceIdPattern.matcher(line);
-            classIdMatcher = classIdPattern.matcher(line);
-            if (!sequenceIdMatcher.find() || !classIdMatcher.find()) {
-                throw new InputMismatchException("Incorrect input format. Expected: <sequenceId>_<classId>_<offset>_<length>");
-            }
-
-            Integer sequenceId = Integer.parseInt(sequenceIdMatcher.group(1));
-            int classId = Integer.parseInt(classIdMatcher.group(1)) - MIN_CLASS;
-            if (!groundTruth.containsKey(sequenceId)) {
-                groundTruth.put(sequenceId, new int[MAX_CLASS - MIN_CLASS + 1]);
-                ++groundTruth.get(sequenceId)[classId];
-            } else {
-                ++groundTruth.get(sequenceId)[classId];
-            }
-        }
-        return groundTruth;
-    }
-
-    /*static private Map<Integer, int[]> parseDataFile() throws IOException {
-        BufferedReader br = new BufferedReader(new FileReader(dataFile));
-        Map<Integer, int[]> groundTruth = new HashMap<>();
-
-        String line;
-        Pattern sequenceIdPattern = Pattern.compile("ObjectKey (\\d+?)_");
-        Pattern motionWordsCountPattern = Pattern.compile("^(\\d+?);");
-        Matcher sequenceIdMatcher, motionWordsCountMatcher;
-        while ((line = br.readLine()) != null) {
-            if (line.contains("#objectKey")) {//TODO refactor parsing
-
-                sequenceIdMatcher = sequenceIdPattern.matcher(line);
-                if (!sequenceIdMatcher.find()) {
-                    throw new InputMismatchException("Incorrect input format. Expected: #objectKey messif.objects.keys.AbstractObjectKey <sequenceId>_0_0_0");
-                }
-                Integer sequenceId = Integer.parseInt(sequenceIdMatcher.group(1));
-
-                line = br.readLine();
-                motionWordsCountMatcher = motionWordsCountPattern.matcher(line);
-                if (!motionWordsCountMatcher.find()) {
-                    throw new InputMismatchException("Incorrect input format. Expected: <motionWordsCount>;mcdr.objects.impl.ObjectMotionWord");
-                }
-                int motionWordsCount = Integer.parseInt(motionWordsCountMatcher.group(1));
-
-                for (int i = 0; i < motionWordsCount; ++i) {
-
-                }
-            }
-        }
-    }*/
-
     public static void main(String[] args) throws IOException {
-        //TODO play with regexes
-        Map<Integer, int[]> groundTruth = parseGroundTruthFile();
+        Map<Integer, List<Integer>> groundTruth = DataLoader.parseGroundTruthFile(groundTruthFile);
+        Map<Integer, List<Integer>> motionsData = DataLoader.parseDataFile(dataFile);
+
+        for (Map.Entry<Integer, List<Integer>> entry : groundTruth.entrySet()) {
+            System.out.println(entry.getKey());
+            System.out.println(entry.getValue());
+        }
+
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+
+
+        for (Map.Entry<Integer, List<Integer>> entry : motionsData.entrySet()) {
+            System.out.println(entry.getKey());
+            System.out.println(entry.getValue());
+        }
     }
 }
