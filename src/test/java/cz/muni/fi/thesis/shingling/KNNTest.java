9a563ec4ae77728d1031cb8e092c446873b50775
Ján Horváth <xhorvat6@fi.muni.cz>
2019-12-22 08:58:28 +0100

    WIP - KNNTests

---
+++ src/test/java/cz/muni/fi/thesis/shingling/KNNTest.java
@@ -0,0 +1,18 @@
+package cz.muni.fi.thesis.shingling;
+
+import org.junit.Before;
+
+import java.util.HashMap;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class KNNTest {
+
+    JaccardMatrix jaccardMatrix;
+
+    @Before
+    public void jaccardMatrixSetup() {
+        jaccardMatrix =
+    }
+
+}
